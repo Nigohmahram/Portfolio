a3abf1c98cf0748f13262f1243c46b53b6c5b73b
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-05-07 16:03:28 +0900

    portfolio site

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+     const navbarDarkIcon = document.querySelector('.navbar-darkmode__icon');
+     navbarDarkIcon.addEventListener('click', function () {
+          document.body.classList.toggle('light')
+     });
+});
